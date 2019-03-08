291571157a09b28ecc0677496b8b4848b669d2d8
FangRui <arnold3000@163.com>
2019-03-08 16:11:47 +0800

    统计图表

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-      HelloWorld
-      <v-chart :options="polar"/>
+    <div class="chartBox">
+      <v-chart class="charts" :options="pie"/>
+    </div>
+    <div class="chartBox">
+      <v-chart class="charts" :options="line"/>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import ECharts from 'vue-echarts'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/component/polar'
+import 'echarts/lib/chart/pie'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/dataZoom'
 
 import changshaData from './assets/data.json'
 
@@ -19,62 +25,174 @@ export default {
     'v-chart': ECharts
   },
   data() {
+    const salaryType = ['面议', '1-5k', '5-8k', '8-10k', '10-13k', '13-15k', '15-20k', '20-25k', '25-30k', '30k+']
     return {
       changshaData,
-      polar: {   
-         title : {
-              text: '分布',
-              subtext: '纯属虚构',
-              x:'center'
-          },
-          tooltip : {
-              trigger: 'item',
-              formatter: "{a} <br/>{b} : {c} ({d}%)"
-          },
-          legend: {
-              orient: 'vertical',
-              left: 'left',
-              data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-          },
-          series : [
-              {
-                  name: '访问来源',
-                  type: 'pie',
-                  radius : '55%',
-                  center: ['50%', '60%'],
-                  data:[
-                      {value:335, name:'直接访问'},
-                      {value:310, name:'邮件营销'},
-                      {value:234, name:'联盟广告'},
-                      {value:135, name:'视频广告'},
-                      {value:1548, name:'搜索引擎'}
-                  ],
-                  itemStyle: {
-                      emphasis: {
-                          shadowBlur: 10,
-                          shadowOffsetX: 0,
-                          shadowColor: 'rgba(0, 0, 0, 0.5)'
-                      }
-                  }
-              }
-          ]
-      }
+      salaryType,
     };
   },
   computed: {
-    data: (chartData) => {
-      return chartData.total * 2
+    CSpie: function (){
+      return this.changshaData.data.map((val, i) => {
+        const re = /^[0-9]+.?[0-9]*/
+        if (re.test(val[5])&&re.test(val[6])){
+         return parseFloat((val[5] + val[6])/2).toFixed(1)
+        }else{
+          // 面议
+          return 0 
+        }
+      })
+    },
+    pie: function() {
+      const data = Array(10).fill(0).map((val, i) => {
+              return { value: val, name: this.salaryType[i] }
+            })
+      this.CSpie.map((val, i) => {
+        if(val<1) data[0].value++
+        if(val>=1&&val<5) data[1].value++
+        if(val>=5&&val<8) data[2].value++
+        if(val>=8&&val<10) data[3].value++
+        if(val>=10&&val<13) data[4].value++
+        if(val>=13&&val<15) data[5].value++
+        if(val>=15&&val<20) data[6].value++
+        if(val>=20&&val<25) data[7].value++
+        if(val>=25&&val<30) data[8].value++
+        if(val>=30) data[9].value++
+      })
+      return {   
+        title : {
+          text: '分布',
+          subtext: '纯属虚构',
+          x:'center'
+        },
+        tooltip : {
+          trigger: 'item',
+          formatter: "{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: this.salaryType
+        },
+        series : [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data: data,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+    },
+    line: function () {
+      const sortedData = this.changshaData.data.sort(this.sortData(true));
+      const lineData = this.CSpie.sort(this.sortData(false));
+      return {
+        tooltip : {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+            label: {
+              show: true
+            }
+          },
+          formatter: function (params){
+            const i = params[0].dataIndex
+            return `
+              ${sortedData[i][0]} <br />
+              ${sortedData[i][1]} <br />
+              ${sortedData[i][4]} <br />
+              ${sortedData[i][5]} - ${sortedData[i][6]} K
+            `
+          }
+        },
+        toolbox: {
+          show : true,
+          feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+          }
+        },
+        calculable : true,
+        legend: {
+          itemGap: 5
+        },
+        grid: {
+          top: '12%',
+          left: '1%',
+          right: '10%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type : 'category',
+            data : sortedData.map((val, i) => {
+              return val[0]
+            })
+          }
+        ],
+        yAxis: [
+          {
+            type : 'value',
+            name : '薪资',
+            axisLabel: {
+
+            }
+          }
+        ],
+        dataZoom: [
+          {
+            show: true,
+            start: 94,
+            end: 100
+          },
+          {
+            type: 'inside',
+            start: 94,
+            end: 100
+          },
+        ],
+        series : [
+          {
+            name: '柱状图排序',
+            type: 'bar',
+            data: lineData
+          },
+        ]
+      }
     }
   },
   methods: {
-
+    sortData: function(isObj) {
+      return function(A, B) {
+        if(isObj){
+          let averageA = 0, averageB = 0;
+          const re = /^[0-9]+.?[0-9]*/
+          if (re.test(A[5])&&re.test(A[6])) averageA = parseFloat((A[5] + A[6])/2).toFixed(1)
+          if (re.test(B[5])&&re.test(B[6])) averageB = parseFloat((B[5] + B[6])/2).toFixed(1)
+          return averageA - averageB
+        }
+        return A - B
+      }
+    }
   },
   beforeCreate() {
     console.log('在实例初始化之后，数据观测 (data observer) 和 event/watcher 事件配置之前被调用');
   },
   created() {
     console.log('在实例创建完成后被立即调用');
-    console.log(this.chartData)
+    console.log(this.CSpie)
   },
   beforeMount() {
     console.log('在挂载开始之前被调用');
@@ -118,5 +236,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.chartBox{
+  width: 100%;
+}
+.charts{
+  width: 100%;
 }
 </style>
