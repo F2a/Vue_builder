6d039205b57a99ab4121c82b08f743335711b6aa
FangRui <arnold3000@163.com>
2019-03-08 11:01:47 +0800

    chart

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,113 @@
 <template>
   <div id="app">
       HelloWorld
+      <v-chart :options="polar"/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/polar'
+
+import changshaData from './assets/data.json'
 
 export default {
-  name: 'app',
-}
+  name: 'Home',
+  components: {
+    'v-chart': ECharts
+  },
+  data() {
+    return {
+      changshaData,
+      polar: {   
+         title : {
+              text: '分布',
+              subtext: '纯属虚构',
+              x:'center'
+          },
+          tooltip : {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+          },
+          series : [
+              {
+                  name: '访问来源',
+                  type: 'pie',
+                  radius : '55%',
+                  center: ['50%', '60%'],
+                  data:[
+                      {value:335, name:'直接访问'},
+                      {value:310, name:'邮件营销'},
+                      {value:234, name:'联盟广告'},
+                      {value:135, name:'视频广告'},
+                      {value:1548, name:'搜索引擎'}
+                  ],
+                  itemStyle: {
+                      emphasis: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      }
+    };
+  },
+  computed: {
+    data: (chartData) => {
+      return chartData.total * 2
+    }
+  },
+  methods: {
+
+  },
+  beforeCreate() {
+    console.log('在实例初始化之后，数据观测 (data observer) 和 event/watcher 事件配置之前被调用');
+  },
+  created() {
+    console.log('在实例创建完成后被立即调用');
+    console.log(this.chartData)
+  },
+  beforeMount() {
+    console.log('在挂载开始之前被调用');
+  },
+  mounted() {
+    console.log('挂载完成');
+  },
+  beforeUpdate() {
+    console.log('数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前');
+  },
+  updated() {
+    console.log('发生在虚拟 DOM 重新渲染和打补丁之后');
+    // 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。然而在大多数情况下，你应该避免在此期间更改状态。
+    this.$nextTick(function () {
+      // 所有的子组件也都一起被挂载可以用 vm.$nextTick
+    })
+  },
+  activated() {
+    console.log('keep-alive 组件激活时调用');
+  },
+  deactivated() {
+    console.log('keep-alive 组件停用时调用');
+  },
+  beforeDestroy() {
+    console.log('Vue 实例销毁之前调用');
+  },
+  destroyed() {
+    console.log('Vue 实例销毁后调用');
+  },
+  errorCaptured(err, vm, info) {
+    console.log('当捕获一个来自子孙组件的错误时被调用');
+  }
+};
 </script>
 
 <style>
